3e8f938b2b31960790d9e7b4e4b60301198e5703
Amir1387107 <amirhossein1387.10.7@gmail.com>
2024-09-13 17:27:33 +0330

    first commit

---
+++ Desktop/ML_Django/ML/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    # path('ML/', views.ML, name='ML'),
+    path('Khodro45/', views.Khodro45, name='Khodro45'),
+    path('Yazd2020/', views.Yazd2020, name='Yazd2020'),
+    path('HamrahMechanic/', views.HamrahMechanic, name='HamrahMechanic'),
+]
